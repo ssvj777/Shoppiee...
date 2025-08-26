d04e2274c0e6154229d5ca019e7965e658c628aa
ssvj777 <ssvj777@gmail.com>
2025-08-26 17:52:07 +0530

    Shoppiee Project Uploaded...

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var sidenav= document.querySelector(".side-navbar")
+
+function showNavbar(){
+    sidenav.style.left = "0"
+}
+
+function closeNavbar(){
+    sidenav.style.left = "-60%"
+}
+
+var container = document.querySelector(".product-section")
+var products = document.querySelectorAll(".products-box")
+
+document.getElementById("search").addEventListener("keyup",function(event){
+    var enterText = event.target.value.toUpperCase()
+    
+    for(product = 0; product < products.length; product ++){
+        if (products[product].querySelector("p").textContent.toUpperCase().indexOf(enterText) < 0){
+            products[product].style.display = "none"
+        }
+        else {
+            products[product].style.display = "block"
+        }
+    }
+})
